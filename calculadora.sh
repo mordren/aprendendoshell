efeabd3acf4f009e71878ec991097421f8512f3b
mordren <joaokko@gmail.com>
2014-02-07 16:45:27 +0000

    Update calculadora.sh
    Mudanca na questao da limpeza da tela e tambem em colocar uma saida mais funcional

--- calculadora.sh
+++ calculadora.sh
@@ -9,11 +9,16 @@ clear
 # calculadora
 while true
 do
+	clear
 	echo -e "Calcularadora" # imprimindo mensagem
 	echo -e "Pressione CTRL+C para encerrar a calculadora\n"
 	read -p "Digite o primeiro número:" numero_1 # pegando resposta do usuário e criando variável
 	read -p "Digite o operador desejado:" operador
 	read -p "Digite o segundo número:" numero_2
 	echo $(($numero_1 $operador $numero_2)) # isso é muito interessante, uma forma indireta de fazer operações
+	read -p "Deseja fazer mais alguma conta? (y/n)" resposta
+	if [ resposta != "y"]; then
+		exit 0
+	fi
 done
 # fim
